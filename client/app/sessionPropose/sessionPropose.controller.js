cd9b0e984f4b8dc40da72bc8e7b6499c2e3c3316
John King <jnkisd@rit.edu>
2016-03-15 23:37:28 -0400

    Basic session proposal front-end, including an empty model and tinymce configuration

--- client/app/sessionPropose/sessionPropose.controller.js
+++ client/app/sessionPropose/sessionPropose.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('rocEventManagerApp')
-  .controller('SessionProposeCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+	.controller('SessionProposeCtrl', function ($scope) {
+		$scope.session = {};
+	
+		$scope.tinymceOptions = {
+			menubar: false,
+			statusbar: true,
+			setup: function(editor) {
+				editor.on("init", function() {
+					this.getDoc().body.style.fontSize = '14px';
+					this.getDoc().body.style.fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
+				});
+			}
+		};
+
+	});
